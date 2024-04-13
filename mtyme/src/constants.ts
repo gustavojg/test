855a4cd413bbf4a56ca70e3f599b21ae803bd716
Gustavo <soygus@gmail.com>
2024-04-13 09:07:56 +0200

    feat create constants

---
+++ mtyme/src/constants.ts
@@ -0,0 +1,12 @@
+export const DAILY: string = "DAILY";
+export const WEEKLY: string = "WEEKLY";
+export const MONTHLY: string = "MONTHLY";
+
+export const TIME_TYPES = {
+  WORK: "WORK",
+  PLAY: "PLAY",
+  STUDY: "STUDY",
+  EXERCISE: "EXERCISE",
+  SOCIAL: "SOCIAL",
+  SELF_CARE: "SELF_CARE",
+};
